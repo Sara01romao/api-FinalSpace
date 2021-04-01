247f81f55d9ac775dea96941845eccdc9b660480
Sara Romão <sara-romao@live.com>
2021-04-01 01:26:13 -0300

    cards

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Cards from './Components/cards';
+import './App.css';
+
+function App() {
+ 
+
+
+  return (
+    <div className="App">
+     <Cards/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/cards.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react'
+import './Cards.css'
+
+
+
+function Cards() {
+
+  const [personagens, setPersonage] =useState([]);
+
+  useEffect(() =>{
+        function loadApi(){
+              let url= "https://finalspaceapi.com/api/v0/character/"
+          
+              fetch(url).then((resposta) => resposta.json()).then((data)=>{
+                console.log(data);
+
+                setPersonage(data);
+              })
+      
+        }
+
+         loadApi();
+  })
+  
+
+
+  return (
+    <div className="cards">
+      {personagens.map((item)=>{
+        return (
+          <div className="card" key={item.id} >
+            <div className="photo">
+                <img src={item.img_url}/>
+
+                <div className="name">
+                <h2>{item.name}</h2>
+                </div>
+               
+            </div>
+            
+            <p>{item.species}</p>
+
+            <div className="descricao">
+                  
+                  <br/>
+                    <h4>Abilities</h4>
+                    <ul>
+                      {item.abilities.map((a)=>{
+                        return (
+                          <li>{a}</li>
+                        )
+                      })}
+                    </ul>
+            </div>
+            
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Cards;
